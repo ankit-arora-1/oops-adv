6a4b5d1ca7cf275de6a91381b5ab630b46d92116
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2024-04-10 10:19:58 +0530

    Added sync

---
+++ src/syncintro/Adder.java
@@ -0,0 +1,15 @@
+package syncintro;
+
+public class Adder implements Runnable {
+    private Count count;
+    public Adder(Count count) {
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 1; i <= 10000; i++) {
+            count.count += i;
+        }
+    }
+}
---
+++ src/syncintro/Count.java
@@ -0,0 +1,5 @@
+package syncintro;
+
+public class Count {
+    public int count = 0;
+}
